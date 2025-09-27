3787561db9a10c0d956bbbab6e182d054e8d31d3
vinhdeptrai-denroidayhihi <vinhhoang14032005@gmail.com>
2025-09-27 23:26:11 +0700

    up code

--- js/main.js
+++ js/main.js
@@ -251,7 +251,7 @@ function renderImage(parent, addClick, length) {
 }
 
 function deleteImage(parent, addClick) {
-    $(parent +' .deleteImg').each((i, value) => {
+    $(parent + ' .deleteImg').each((i, value) => {
         $(value).click(() => {
             $(value).closest('.parent-img').remove()
             $(parent + addClick).removeClass('d-none');
@@ -336,6 +336,11 @@ $('#addClassifyProduct').click(() => {
         $('#addClassify').removeClass('d-none');
         $('#addClassify').append(formClassify(indexClassify));
         $('#tableClassify').removeClass('d-none');
+        if(indexClassify == 1){
+            $('.classification_2').each((i, v)=> {
+                $(v).removeClass('d-none');
+            })
+        }
         indexClassify++;
         handleDeleteClassify();//gọi hàm xóa biến thể
         autoAddInput();//gọi hàm khởi đông tạo biến thể
@@ -346,20 +351,25 @@ $('#addClassifyProduct').click(() => {
 $(document).on("click", ".closeFormClassify", function () {
     $(this).closest(".classifyParent").remove();
     indexClassify--;
+    
     if (indexClassify == 0) {
         $('#addClassify').addClass('d-none');
         $('#tableClassify').addClass('d-none');
         $('#formPrice').removeClass('d-none');
+    }else{
+        $('#addClassify .classifyParent').find('.titleClassify').text('Phân loại 1');
+        $('#addClassify .classifyParent').find('.classifyName').prop('name', 'classifyName' + 0);
+        $('#addClassify .classifyParent').find('.classify').each((i, val) => {
+            $(val).prop('name', 'classify' + 0 + '[]');
+        });
+        $('.addElement').prop('id', 'addElement' + 0);
+        deleteColumn();
+        $('#formClassify1').prop('id', 'formClassify' + 0);
+        autoAddInput();//gọi hàm khởi đông tạo biến thể
+        handleEventAddClassify();//gọi hàm xử lý value classify
     }
-    $('#addClassify .classifyParent').find('.titleClassify').text('Phân loại 1');
-    $('#addClassify .classifyParent').find('.classifyName').prop('name', 'classifyName' + 0);
-    $('#addClassify .classifyParent').find('.classify').each((i, val) => {
-        $(val).prop('name', 'classify' + 0 + '[]');
-    });
-    $('.addElement').prop('id', 'addElement' + 0);
-    autoAddInput();//gọi hàm khởi đông tạo biến thể
-    handleEventAddClassify();//gọi hàm xử lý value classify
 });
+
 
 function autoAddInput() {
     const inputElement = $('.classifyParent');
@@ -371,7 +381,31 @@ function autoAddInput() {
             });
     });
 }
+
+function deleteColumn() {
+    if($('#formClassify1').length){
+
+    }else {
+        $('.classification_2').each((i, v)=> {
+            if(i == 0) {
+                $(v).addClass('d-none');
+                $('#addDataTable .parent' + i).children(':nth-child(2)').addClass('d-none');
+                $('#addDataTable .parent' + i).children(':nth-child(2)').text('');
+            }else {
+                $('#addDataTable .parent' + i).each((j, val)=> {
+                    $(val).children(':nth-child(2)').addClass('d-none');
+                });
+                $('#addDataTable .children' + i).each((j, val)=> {
+                    $(val).remove();
+                });
+            }
+        })
+    }
+}
+
+
 var arr = [];
+var checkAddInput = false;
 function recursionInput(i) {
     const length = $('#addElement' + i + ' .col-lg-6').length - 1;
     $('#addElement' + i + ' .col-lg-6').each((j, value) => {
@@ -379,41 +413,61 @@ function recursionInput(i) {
             $(value).find('.classify')
                 .off('input')
                 .on('input', () => {
-                    if(i == 0) {
-                        if($('#addDataTable .parent' + j).length){
-                            $('#addDataTable .parent' + j).children().first().text($(value).find('.classify').val());
-                        }else{
-                            $('#addDataTable').append(tableClassify($(value).find('.classify').val(),'', '', 'parent' + j));
-                            arr.push(j);
+                    if ($(value).find('.classify').val().trim()) {
+                        if (i == 0) {
+                            if ($('#addDataTable .parent' + j).length) {
+                                $('#addDataTable .parent' + j).children().first().text($(value).find('.classify').val().trim());
+                            } else {
+                                $('#addDataTable').append(tableClassify($(value).find('.classify').val().trim(), '', '', 'd-none', 'parent' + j));
+                                if ($('#addElement' + 1).length) {
+                                    $('#addElement' + 1 + ' .col-lg-6').each((e, value) => {
+                                        if ($(value).find('.classify').val()) {
+                                            if (e == 0) {
+                                                console.log($('#addDataTable .parent' + j));
+                                                $('#addDataTable .parent' + j).children(':nth-child(2)').text($(value).find('.classify').val().trim());
+                                            } else {
+                                                $('#addDataTable .parent' + j).after(tableClassify('', $(value).find('.classify').val().trim(), 'd-none', '', 'children' + j));
+                                                $('#addDataTable .parent' + j).children().first().prop('rowspan', (e + 1));
+                                            }
+                                        }
+                                    });
+                                }
+                                arr.push(j);
+                            }
                         }
-                    }
-                    if(i == 1) {
-                        arr.forEach((i, val)=> {
-                                $(value).find('.classify').each((v, val)=> {
-                                    if($(val).val()){
-                                        // console.log($('#addDataTable .children' + j).length);
-                                        // console.log($('#addDataTable .children' + j));
-                                        if($('#addDataTable .children' + j).length){
-                                            $('#addDataTable .children' + j).children(':nth-child(2)').text($(val).val());
-                                        }
-                                        else {
-                                            $('#addDataTable .parent' + i).after(tableClassify('', $(val).val(), '', 'children' + j));
-                                        }
-                                        console.log(i);
-                                    }
-                                })
-                        })
+                        if (i == 1) {
 
-                    }
-                    if (j == $('#addElement' + i + ' .col-lg-6').length - 1) {
-                        $('#addElement' + i).append(inputElementText(i));
-                        // thêm nút xóa biến thể đầu tiên
-                        if($('#addElement' + i).find('.deleteClassify').length < 2){
-                            const iconDeletClassify = '<ion-icon class="ms-3 deleteClassify" style="font-size: 25px; color: rgb(182, 127, 127); cursor: pointer;" name="trash-outline"></ion-icon>';
-                            $('#addElement' + i).children().first().find('.addDelete').append(iconDeletClassify);
+                            $(value).find('.classify').each((v, values) => {
+                                if ($(values).val()) {
+                                    if ($('#addDataTable .children' + j).length) {
+                                        $('#addDataTable .children' + j).children(':nth-child(2)').text($(values).val().trim());
+                                    } else {
+
+                                        if (j == 0) {
+                                            arr.forEach((element, e) => {
+                                                $('#addDataTable .parent' + e).children(':nth-child(2)').text($(values).val().trim())
+                                            });
+                                        } else {
+                                            arr.forEach((element, e) => {
+                                                $('#addDataTable .parent' + e).after(tableClassify('', $(values).val().trim(), 'd-none', '', 'children' + j));
+                                                $('#addDataTable .parent' + e).children().first().prop('rowspan', (j + 1));
+                                            });
+                                        }
+                                    }
+                                }
+                            })
+
                         }
-                        handleDeleteClassify();//gọi hàm xóa biến thể
-                        recursionInput(i);// hàm gọi tạo biến thể (Đệ quy)
+                        if (j == $('#addElement' + i + ' .col-lg-6').length - 1) {
+                            $('#addElement' + i).append(inputElementText(i));
+                            // thêm nút xóa biến thể đầu tiên
+                            if ($('#addElement' + i).find('.deleteClassify').length < 2) {
+                                const iconDeletClassify = '<ion-icon class="ms-3 deleteClassify" style="font-size: 25px; color: rgb(182, 127, 127); cursor: pointer;" name="trash-outline"></ion-icon>';
+                                $('#addElement' + i).children().first().find('.addDelete').append(iconDeletClassify);
+                            }
+                            handleDeleteClassify();//gọi hàm xóa biến thể
+                            recursionInput(i);// hàm gọi tạo biến thể (Đệ quy)
+                        }
                     }
                 });
         }
@@ -421,7 +475,7 @@ function recursionInput(i) {
 }
 
 function handleDeleteClassify() {
-    $('.deleteClassify').each((i, vall) => {
+    $('.deleteClassify').each((d, vall) => {
         $(vall)
             .off('click')
             .on('click', () => {
@@ -431,8 +485,10 @@ function handleDeleteClassify() {
                         .off('click')
                         .on('click', () => {
                             $(val).find(vall).closest('.col-lg-6').remove();
+                            // $('#addDataTable .parent' + i)
+                            console.log(d);
                             // xóa nút xóa biến thể
-                            if($('#addElement' + i).find('.deleteClassify').length < 2){
+                            if ($('#addElement' + i).find('.deleteClassify').length < 2) {
                                 $('#addElement' + i).children().first().find('.deleteClassify').remove();
                             }
                             autoAddInput();
@@ -448,7 +504,7 @@ function formClassify(i) {
             <div class="col-lg-2 col-12 titleClassify">Phân loại ${i + 1}</div>
             <div class="col-lg-10 col-12">
                 <!-- thêm phân loại-->
-                <div class="d-md-flex" id="">
+                <div class="d-md-flex" id="formClassify${i}">
                     <div class="bg-secondary bg-opacity-10 w-100 rounded-2">
                         <div class="d-flex justify-content-between p-3 border-bottom">
                             <div class="d-flex align-items-center classifyNameParent">
@@ -511,33 +567,34 @@ function inputElementText(i) {
 
 
 function handleEventAddClassify() {
-    $('.successClassify').each((i, classifyName)=> {
+    $('.successClassify').each((i, classifyName) => {
         $(classifyName)
-        .off('click')
-        .on('click', ()=> {
-            if(i == 0) {
-                const parent = $(classifyName).closest('.classifyNameParent');
-                parent.find('.classifyName').parent().addClass('d-none');
-                parent.find('.textClassify p').text(parent.find('.classifyName').val());
-                parent.find('.textClassify').removeClass('d-none');
-                $('.classify_1').text(parent.find('.classifyName').val());
-            }else {
-                const parent = $(classifyName).closest('.classifyNameParent');
-                parent.find('.classifyName').parent().addClass('d-none');
-                parent.find('.textClassify p').text(parent.find('.classifyName').val());
-                parent.find('.textClassify').removeClass('d-none');
-                $('.classify_2').text(parent.find('.classifyName').val());
-            }
-        });
+            .off('click')
+            .on('click', () => {
+                if (i == 0) {
+                    const parent = $(classifyName).closest('.classifyNameParent');
+                    parent.find('.classifyName').parent().addClass('d-none');
+                    parent.find('.textClassify p').text(parent.find('.classifyName').val());
+                    parent.find('.textClassify').removeClass('d-none');
+                    $('.classify_1').text(parent.find('.classifyName').val());
+                } else {
+                    const parent = $(classifyName).closest('.classifyNameParent');
+                    parent.find('.classifyName').parent().addClass('d-none');
+                    parent.find('.textClassify p').text(parent.find('.classifyName').val());
+                    parent.find('.textClassify').removeClass('d-none');
+                    $('.classify_2').text(parent.find('.classifyName').val());
+                }
+            });
     });
 }
 
 
-function tableClassify(value1, value2, index, location) {
+function tableClassify(value1, value2, hidden1, hidden2, location) {
     return `
+    
         <tr class="${location}">
-            <td rowspan="${index}" class="text-wrap text-break" style="max-width: 200px">${value1}</td>
-            <td >${value2}</td>
+            <td rowspan="" class="text-wrap text-break ${hidden1}" style="max-width: 200px">${value1}</td>
+            <td class="classification_2 ${hidden2 == '' ? hidden2 : 'd-none'}">${value2}</td>
             <td>
                 <input type="text" name="sellingPrice"
                     onkeypress="return event.charCode >= 48 && event.charCode <= 57" readonly
